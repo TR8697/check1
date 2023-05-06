be2b08da3bef7e5322707c79cc99a3746e305a13
TR8697 <128630242+TR8697@users.noreply.github.com>
2023-05-06 23:50:08 +0300

    Add files via upload

---
+++ Check/src/Check/Main.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Check;
+
+use Check\command\CheckCommand;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase
+{
+    public static Main $main;
+    
+    public function onLoad():void
+    {
+        self::$main = $this;
+    }
+    protected function onEnable(): void
+    {
+        $this->getLogger()->info("Çek aktif - ayd4ndemirci");
+        $this->getServer()->getCommandMap()->register("cek", new CheckCommand());
+        $this->getServer()->getPluginManager()->registerEvents(new CheckListener(), $this);
+    }
+}
+?>
